c9ca1e717410752add7ead22bd424e1d3c003f1f
loryplayer <londerolory@libero.it>
2021-12-26 21:07:42 +0100

    Fixed function to alignment the objects on canvas

--- Draw.py
+++ Draw.py
@@ -59,6 +59,7 @@ class Draw:
                 self.object_on_layer = []
                 self._x_pointer = initial_x
                 self.y_pointer = 75
+                self.y_last_pointer = 0
 
             def draw_neuron(self, neuron: Neuron):
                 x0 = self._x_pointer
@@ -68,10 +69,18 @@ class Draw:
                                                                       fill=self.neuron_colors)
                 neuron_name = self.draggable_frame.canvas.create_text(x0, y0 - 5,
                                                                       text=neuron.name,
-                                                                      font=("Purisa", 12))
+                                                                      font=("Purisa", 11))
                 neuron_sigma = self.draggable_frame.canvas.create_text(x0, y0 + 15,
                                                                        text=round(neuron.sigma),
-                                                                       font=("Purisa", 12))
+                                                                       font=("Purisa", 12, 'bold'))
+                if neuron.bias is not None:
+                    neuron_bias = self.draggable_frame.canvas.draw_txt(x0, y0 - 45,
+                                                                       text=neuron.bias[1] if neuron.bias[
+                                                                                                  1] < 0 else '+' + str(
+                                                                           neuron.bias[1]),
+                                                                       font=("Purisa", 10))
+                    self.object_on_layer.append(neuron_bias)
+                self.y_last_pointer = self.y_pointer
                 self.y_pointer = y1 + self.radius * 3
                 self.neurons.update({neuron_drew: neuron})
                 self.object_on_layer.append(neuron_drew)
@@ -86,20 +95,17 @@ class Draw:
             def x_pointer(self):
                 return self._x_pointer
 
+            def write_name_layer(self, x, y):
+                self.draggable_frame.canvas.create_text(x, y, text=self.name.upper(),
+                                                        font=("Purisa", 14, "bold italic"))
+
         def get(self, name: str) -> Layer:
             for layer in self.layers:
                 if layer.name == name:
                     return layer
 
         def connect_neurons(self):
-            # self.draggable_frame.canvas.draw_circle(23.206933092733937, 414.63445352581283, 5, outline='red',
-            #                                        consider=False)
-            # self.draggable_frame.canvas.draw_circle(124.79306690726607,
-            #                                        333.36554647418717, 5, outline='red', consider=False)
-            # self.draggable_frame.canvas.draw_line(-1,434, 124.79306690726607,
-            #                                      333.36554647418717,
-            #                                      arrow=tk.LAST, show_intersections=True)
-            aaa = True
+            deltaY, deltaX = 0, 0
             for k, layer in enumerate(self.layers):
                 for neuron_info in layer.neurons.items():
                     neuron_coords = (self.draggable_frame.canvas.bbox(neuron_info[0])[0] + self.radius,
@@ -114,14 +120,16 @@ class Draw:
                                     neuron_connected_ += (
                                         self.draggable_frame.canvas.bbox(next_neuron[0])[0] + self.radius,
                                         self.draggable_frame.canvas.bbox(next_neuron[0])[1] + self.radius)
+                        deltaY = (neuron_coords[1] - neuron_connected_[1])
+                        deltaX = (neuron_connected_[0] - neuron_coords[0])
+                        angle = (math.atan2(deltaY, deltaX))
+                        x_offset = (self.radius * 2 * math.cos(angle))
+                        y_offset = (self.radius * 2 * math.sin(angle))
                         weight = self.draggable_frame.canvas.draw_txt(
-                            neuron_coords[0] + (neuron_connected_[0] - neuron_coords[0]) / 2,
-                            neuron_coords[1] + (neuron_connected_[1] - neuron_coords[1]) / 2 + 15,
-                            text=weight_value,
-                            font=("Purisa", 12))
-            # self.draggable_frame.canvas.draw_line(-1,194,268.6019990535815,247.9203998107163,
-            #                                     arrow=tk.LAST, show_intersections=True)
-
+                            neuron_coords[0] + x_offset,
+                            neuron_coords[1] - y_offset,
+                            text=weight_value if weight_value < 0 else '+' + str(weight_value),
+                            font=("Purisa", 8))
             for k, layer in enumerate(self.layers):
                 for neuron_info in layer.neurons.items():
                     neuron_coords = (self.draggable_frame.canvas.bbox(neuron_info[0])[0] + self.radius,
@@ -134,43 +142,50 @@ class Draw:
                                     neuron_connected_ += (
                                         self.draggable_frame.canvas.bbox(next_neuron[0])[0] + self.radius,
                                         self.draggable_frame.canvas.bbox(next_neuron[0])[1] + self.radius)
-                        deltaY = (neuron_coords[1] - neuron_connected_[1])
-                        deltaX = (neuron_connected_[0] - neuron_coords[0])
-                        xy = self.draggable_frame.canvas.bbox(neuron_info[0])
-                        # print(xy)
-                        x = xy[2] - xy[0]
-                        # print(x / 2)
-                        angle = (math.atan2(deltaY, deltaX))
-                        x_offset = ((x / 2) * math.cos(angle))
-                        y_offset = ((x / 2) * math.sin(angle))
                         line_xa = neuron_coords[0]
                         line_ya = neuron_coords[1]
+                        angle = (math.atan2(deltaY, deltaX))
+                        xy = self.draggable_frame.canvas.bbox(neuron_info[0])
+                        x = xy[2] - xy[0] + 1
+                        x_offset = ((x / 2) * math.cos(angle))
+                        y_offset = ((x / 2) * math.sin(angle))
                         line_xb = neuron_connected_[0] - x_offset
                         line_yb = neuron_connected_[1] + y_offset
-
-                        # self.draggable_frame.canvas.draw_circle(line_xa, line_ya, 5, outline='red', consider=False)
-                        # self.draggable_frame.canvas.draw_circle(line_xb, line_yb, 5, outline='red', consider=False)
-                        # print(neuron_info[1].name)
-                        # print(line_xa, line_ya, line_xb, line_yb, sep=',')
                         self.draggable_frame.canvas.draw_line(line_xa, line_ya, line_xb, line_yb,
                                                               arrow=tk.LAST, offset_collision=5,
                                                               show_intersections=False)
 
         def adjust_neurons(self):
+            for k, layer in enumerate(self.layers):
+                layer.move_objects(-layer.y_last_pointer / 2)
             max_y = 0
+            min_y = self.draggable_frame.canvas.bbox(self.layers[0].object_on_layer[0])[3]
             for k, layer in enumerate(self.layers):
-                for neuron_info in layer.neurons.items():
-                    # print(neuron_info)
-                    if self.draggable_frame.canvas.bbox(neuron_info[0])[3] > max_y:
-                        max_y = self.draggable_frame.canvas.bbox(neuron_info[0])[3]
-            # print(max_y)
+                for obj in layer.object_on_layer:
+                    if self.draggable_frame.canvas.bbox(obj)[3] > max_y:
+                        max_y = self.draggable_frame.canvas.bbox(obj)[3]
+                    if self.draggable_frame.canvas.bbox(obj)[1] < min_y:
+                        min_y = self.draggable_frame.canvas.bbox(obj)[1]
+            min_x = self.layers[0].x_pointer - self.layers[0].radius
+            max_x = self.layers[len(self.layers) - 1].x_pointer + self.layers[len(self.layers) - 1].radius
+            padding = 50
+            min_x -= padding
+            max_x += padding
+            min_y -= padding
+            max_y += padding
+            delta_layer_x = max_x - min_x
+            delta_layer_y = max_y - min_y
+            frame = self.draggable_frame
+            frame.update()
+            frame.update_idletasks()
+            delta_x = frame.winfo_width() - delta_layer_x
+            delta_y = frame.winfo_height() - delta_layer_y
+            a = (min_x, min_y, max_x, max_y)
+            frame.canvas.create_rectangle(*a)
             for k, layer in enumerate(self.layers):
-                max_y_for_this_layer = 0
-                for neuron_info in layer.neurons.items():
-                    if self.draggable_frame.canvas.bbox(neuron_info[0])[3] > max_y_for_this_layer:
-                        max_y_for_this_layer = self.draggable_frame.canvas.bbox(neuron_info[0])[3]
-                # print(max_y - max_y_for_this_layer)
-                layer.move_objects((max_y - max_y_for_this_layer) / 2)
+                layer.write_name_layer(layer.x_pointer, min_y - delta_y)
+            for object_id in frame.canvas.find_all():
+                frame.canvas.move(object_id, - min_x + delta_x / 2, - min_y + delta_y / 2)
 
     def go(self):
         self.draggable_frame.set_scrollregion(*self.draggable_frame.get_more_distant_points())

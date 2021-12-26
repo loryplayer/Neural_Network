de070ebf607a61dc02159e1a7e302fccabf73e1c
loryplayer <londerolory@libero.it>
2021-12-26 21:00:23 +0100

    Created class to handle the object created on canvas, created functions to calculate the position of intersection of the lines drew on canvas

--- Draggable_Frame.py
+++ Draggable_Frame.py
@@ -8,8 +8,6 @@ class Vector2_extended(Vector2):
         Vector2.__init__(self, *args)
 
     def is_in_range(self, pa, pb):
-        # print(self, pa, pb, sep=', ')
-        '''if pa.y < pb.y else pb.y  if pb.y > pa.y else pa.y'''
         if pa.x <= self.x <= pb.x and (pa.y if pa.y < pb.y else pb.y) <= self.y <= (pb.y if pb.y > pa.y else pa.y):
             return True
         return False
@@ -54,40 +52,23 @@ class Canvas(tk.Canvas):
         line_coord_of_collisions = []
         a = y1 - y0
         b = x0 - x1
-        ab = (y1 - y0, x1 - x0)
         c = y0 * x1 - x0 * y1
-        # print(f"a: {a}, b: {b}, c: {c}")
         angle = (math.atan2(y1 - y0, x1 - x0))
-        # print(angle)
         m = -a / b
         q = -c / b
-        line_xy = [(x0, x1), (y0, y1)]
         for object_id in self.objects.get('q') + self.objects.get('t'):
             bbox = self.bbox(object_id)
-            # print(f"bbox: {bbox}")
-            # print(f"NUOVO OGGETTO: {object_id}")
             for n_ in range(2):
                 for k in range(2):
-                    # print(2 * k)
                     if n_ == 1 and m == 0: break
                     value = (bbox[2 * k] * m + q) if n_ == 0 else ((bbox[1 + 2 * k] - q) / m)
-                    # print(
-                    #    f"bbox: {bbox[n_ + 2 * k]}; ab: {ab}; linea: {line_xy[n_][0 if ab[n_] > 0 else 1]}; "
-                    #    f"lineb: {line_xy[n_][1 if ab[n_] > 0 else 0]}")
-
                     if Vector2_extended(
                             Vector2((bbox[2 * k], value) if n_ == 0 else (value, bbox[1 + 2 * k]))).is_in_range(
                         Vector2(x0, y0), Vector2(x1, y1)):
-                        # print("dentro")
                         if bbox[1 - n_] <= value <= bbox[3 - n_]:
-                            # print("collisione")
-                            # radius = 5
                             line_coord_of_collisions.append((bbox[2 * k], value) if n_ == 0 else (
                                 value, bbox[1 + 2 * k]))
 
-        # print(self.objects.get('q') + self.objects.get('t'))
-        # def is_in_range(point: Vector2, : Vector2, P2: Vector2):
-        # print(line_coord_of_collisions)
         for object_id in self.objects.get('c'):
             bbox = self.bbox(object_id)
             radius = abs(bbox[2] - bbox[0]) / 2
@@ -109,23 +90,14 @@ class Canvas(tk.Canvas):
                 t2 = (-b_circle - sqrt_disc) / (2 * a_circle)
                 point_a: [Vector2_extended] = Vector2_extended(P1 + t1 * V)
                 point_b: [Vector2_extended] = Vector2_extended(P1 + t2 * V)
-                # if P1 >= point_a >= P2 and P1 >= point_b >= P2:
                 if point_a.is_in_range(P1, P2):
                     line_coord_of_collisions.append((point_a[0], point_a[1]))
-                    # self.draw_circle(point_a[0], point_a[1], 5, consider=False)
-                # else:
-                # print(f"Punto a: {point_a}")
                 if point_b.is_in_range(P1, P2):
                     line_coord_of_collisions.append((point_b[0], point_b[1]))
-                    # self.draw_circle(point_b[0], point_b[1], 5, consider=False)
-                # else:
-                # print(f"Punto b: {point_b}")
         line_coord_of_collisions.sort(reverse=False if (x0, y0) < (x1, y1) else True)
-        # print(line_coord_of_collisions)
-
         if show_intersections:
             for coords in line_coord_of_collisions:
-                print(coords)
+                # print(coords)
                 self.draw_circle(*coords, 5, consider=False)
 
         def adder(list_obj: tuple, value_: float) -> tuple:
@@ -134,9 +106,7 @@ class Canvas(tk.Canvas):
             value_sin = value_ * math.sin(angle)
             for k, item in enumerate(list_obj):
                 val = value_cos if k == 0 else value_sin
-                # print(val)
                 diff += (item + val,)
-            # self.draw_circle(*(109, 84.17931034482758), 5, outline='red', consider=False)
             return diff
 
         def check_collision(point):
@@ -155,19 +125,9 @@ class Canvas(tk.Canvas):
 
         line_coord_of_collisions_fixed = []
         for k, tuple_ in enumerate(line_coord_of_collisions):
-            # print((1 if k % 2 == (0 if angle > 0 else 1) else -1))
-            # print(f"check_collision(Vector2({x0}, {y0})): {check_collision(Vector2(x0, y0))}")
             line_coord_of_collisions_fixed.append(
                 adder(tuple_, offset_collision * (
                     1 if k % 2 == 0 else -1)))
-        # print(line_coord_of_collisions_fixed)
-
-        # self.create_oval(*line_coord_of_collisions[0], outline="red")
-        # self.create_oval(*line_coord_of_collisions[1], outline="red")
-        # self.create_line(x0, y0, x1, y1, **kwargs)
-
-        # self.draw_circle(x0, y0, 5, consider=False)
-        # print(check_collision(Vector2(x0, y0)))
         divided_coordinates = [] if check_collision(Vector2(x0, y0)) else [(x0, y0)]
         divided_coordinates += line_coord_of_collisions_fixed + [adder((x1, y1), -offset_collision)]
         divided_coordinates.sort(reverse=False if (x0, y0) < (x1, y1) else True)
@@ -176,7 +136,6 @@ class Canvas(tk.Canvas):
         for k, coords in enumerate(divided_coordinates):
             temp_coords += coords
             if k % 2 == 1:
-                # print(temp_coords)
                 line_created.append(
                     self.create_line(*temp_coords, arrow=arrow if k == len(divided_coordinates) - 1 else ''))
                 temp_coords = ()
@@ -190,9 +149,6 @@ class Draggable_frame(tk.Frame):
         self.xsb = tk.Scrollbar(self, orient="horizontal", command=self.canvas.xview)
         self.ysb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
         self.canvas.configure(yscrollcommand=self.ysb.set, xscrollcommand=self.xsb.set)
-
-        # self.xsb.grid(row=1, column=0, sticky="ew")
-        # self.ysb.grid(row=0, column=1, sticky="ns")
 
         self.canvas.pack(expand=True, fill=tk.BOTH)  # .grid(row=0, column=0, sticky="nsew")
         self.grid_rowconfigure(0, weight=1)
